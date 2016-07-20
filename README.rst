8a71c3f8bbc50c30cc747ccc9706ca6554fd022d
GermainZ <germanosz@gmail.com>
2016-07-20 12:46:52 +0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,176 @@
+Taskerbot
+=========
+
+A reddit bot to make moderation easier, especially on mobile.
+
+Its main goal is to allow mods to remove posts while leaving removal reasons
+via the bot.
+
+Usage
+=====
+
+Taskerbot can be invoked by any comment containing one of the following
+commands (**all arguments are required**):
+
+- ``@rule {reason}``: **removes a thread, leaving an appropriate flair and
+  comment**. ``reason`` is one of the removal reasons' keys (see
+  `Removal reasons`_). If no corresponding removal reason is
+  found, the ``Generic`` reason is used instead.
+
+  Example: ``@rule 1``, ``@rule spam``.
+
+- ``@ban {duration} "{reason}" "{message}"``: **bans the comment/thread's
+  author**. All three arguments are required. ``duration`` is the ban's
+  duration in days, ``reason`` is the ban's reason (visible only to mods), and
+  ``message`` is the message the banned user will receive. ``reason`` and
+  ``message`` must be between quotations (``""```) (escapes are not supported).
+
+  Example: ``@ban 3600 "spammer" "repeatedly spamming somedomain.com"``
+
+Multiple actions can be specified at once. Taskerbot will automatically remove
+the comment that invoked it.
+
+- **Refreshing the list of moderators/removal reasons**:
+
+  Taskerbot loads the subreddit's list of moderators and removal reasons at
+  startup. To refresh these, send Taskerbot a message containing ``@refresh
+  Subreddit`` (e.g. ``@refresh Android`` to reload ``/r/Android``'s
+  configuration).
+
+  Note that this is case sensitive, so make sure it's the same as what's in the
+  configuration file (see `Configuration file`_).
+
+Only comments made by/mails sent by the subreddit's moderators are checked.
+
+Setup
+=====
+
+Dependencies
+------------
+
+Taskerbot requires Python 3. For a list of required Python 3 libraries, see
+``requirements.txt``.
+
+The required Heroku__ files are provided with this project if you're interested
+in running it on a Heroku app. For example (after setup):
+
+.. code:: py
+
+   heroku ps:scale bot=1 --app app-name-here
+
+__ https://heroku.com/
+
+Requirements
+------------
+
+``/u/Taskerbot`` doesn't accept new additions as we want to keep an acceptable
+load. That being said, you're free to run your own instance.
+
+You'll first want to create a new account and make it a mod of your subreddit.
+Required permissions are: access, flair, posts, and wiki.
+
+Configuration
+-------------
+
+Taskerbot uses `YAML`_ for its configuration files. You can find a good
+document covering the syntax here__.
+
+__ https://docs.ansible.com/ansible/YAMLSyntax.html
+
+.. _Configuration file:
+
+Configuration file
+~~~~~~~~~~~~~~~~~~
+
+The ``config.yaml`` file must contain the bot's username, password and a list
+of subreddits to patrol. The syntax is as follows:
+
+.. code:: yaml
+
+   User Agent: 'user agent here'
+   Username: 'bot username here'
+   Password: 'bot password here'
+   Subreddits:
+       - 'SomeSubreddit'
+       - 'AnotherSubreddit'
+       - 'YetAnotherSub'
+
+Use a `YAML validator`_ to make sure the configuration file is valid.
+
+.. _Removal reasons:
+
+Removal reasons
+~~~~~~~~~~~~~~~
+
+This step must be performed for every new subreddit you want Taskerbot to
+patrol.
+
+1. Create a new wiki page on the subreddit called ``taskerbot``.
+   For example, if you want Taskerbot to patrol ``/r/Android``, you must create
+   ``/r/Android/wiki/taskerbot``.
+2. Fill the page with the reasons you want -- ``Header``, ``Footer`` and
+   ``Generic`` are required reasons so make sure you include at least those:
+
+   .. code:: yaml
+
+      Header: "Sorry {author}, your submission has been removed:"
+
+      Footer: "If you would like to appeal, please message the moderators. *I am a bot, but this message was generated at the instruction of a human moderator.*"
+
+      Generic:
+
+          Flair: 'Removed'
+
+          Message: |
+              Please review our sidebar for the complete list of rules.
+
+      1:
+
+          Flair: "Removed (Rule 1)"
+
+          Message: |
+              Sorry, your post was removed as it breaks rule 1!
+              Check our wiki for more info.
+
+      2:
+
+          Flair: "Removed (Rule 2)"
+
+          Message: |
+              Sorry, your post was removed as it breaks rule 2!
+              Check our wiki for more info.
+
+              Also consider checking some of our sister subreddits if you want to
+              do XYZ:
+
+              - /r/SomeSubreddit - for X.
+              - /r/SomeOtherSubreddit - for Y.
+              - /r/YetAnotherSubreddit - for Z.
+
+      spam:
+
+          Flair: "Removed (Spam)"
+
+          Message: |
+              Sorry, your post was removed as we don't like spam!
+              Check our wiki for more info.
+
+   **Reasons' keys cannot contain spaces** (e.g. in the example above, ``1``
+   and ``spam`` are fine, but ``reason 2`` is not).
+
+   Each custom removal reason must have two entries: ``Flair``, which will be
+   what the removed thread's flair is set to, and ``Message``, which is the
+   comment Taskerbot will leave in the thread.
+
+   Also note that Taskerbot will automatically replace all instances of
+   ``{author}`` in the ``Header`` and ``Footer`` with the author's username.
+
+   You can check `/r/Android's taskerbot wiki page`__ for a real example (click
+   "View source" in the bottom right).
+
+   __ https://www.reddit.com/r/Android/wiki/taskerbot
+
+Use a `YAML validator`_ to make sure the configuration file is valid.
+
+.. _YAML validator: http://www.yamllint.com/
+.. _YAML: http://www.yaml.org/
---
+++ taskerbot.py
@@ -0,0 +1,142 @@
+import html
+import logging
+import re
+import sys
+import time
+
+from praw import Reddit
+import yaml
+
+
+class Bot(object):
+
+    def __init__(self, r):
+        self.r = r
+        logging.debug('Logging in…')
+        self.r.login(USERNAME, PASSWORD)
+        logging.debug('Success.')
+        self.subreddits = {}
+        for subreddit in SUBREDDITS:
+            logging.debug('Checking subreddit: %s…', subreddit)
+            self.subreddits[subreddit] = {}
+            sub = self.subreddits[subreddit]
+            logging.debug('Loading mods…')
+            sub['mods'] = list(redditor.name for redditor in
+                               self.r.get_moderators(subreddit))
+            logging.debug('Mods loaded: %s.', sub['mods'])
+            logging.debug('Loading reasons…')
+            sub['reasons'] = yaml.load(html.unescape(
+                self.r.get_wiki_page(subreddit, 'taskerbot').content_md))
+            logging.debug('Reasons loaded.')
+
+    def refresh_sub(self, subreddit):
+        logging.debug('Refreshing subreddit: %s…', subreddit)
+        sub = self.subreddits[subreddit]
+        logging.debug('Loading mods…')
+        sub['mods'] = list(redditor.name for redditor in
+                           self.r.get_moderators(subreddit))
+        logging.debug('Mods loaded: %s.', sub['mods'])
+        logging.debug('Loading reasons…')
+        sub['reasons'] = yaml.load(html.unescape(
+            self.r.get_wiki_page(subreddit, 'taskerbot').content_md))
+        logging.debug('Reasons loaded.')
+
+    def check_comments(self, subreddit):
+        logging.debug('Checking subreddit: %s…', subreddit)
+        sub = self.subreddits[subreddit]
+        for comment in self.r.get_comments(subreddit, limit=100):
+            if comment.banned_by or comment.author.name not in sub['mods']:
+                continue
+
+            # Check for @rule command.
+            match = re.search(r'@rule (\w*)', comment.body)
+            if match:
+                rule = match.group(1)
+                logging.debug('Rule %s matched.', rule)
+                if rule not in sub['reasons']:
+                    rule = 'Generic'
+                msg = sub['reasons'][rule]['Message']
+
+                parent = self.r.get_info(thing_id=comment.parent_id)
+                comment.remove()
+                parent.remove()
+
+                if parent.fullname.startswith('t3_'):
+                    logging.debug('Removed submission.')
+                    header = sub['reasons']['Header'].format(
+                        author=parent.author.name)
+                    footer = sub['reasons']['Footer'].format(
+                        author=parent.author.name)
+                    msg = '{header}\n\n{msg}\n\n{footer}'.format(
+                        header=header, msg=msg, footer=footer)
+                    parent.add_comment(msg).distinguish()
+                    parent.set_flair(sub['reasons'][rule]['Flair'])
+                elif parent.fullname.startswith('t1_'):
+                    logging.debug('Removed comment.')
+
+            # Check for @ban command.
+            match = re.search(r'@ban (\d*) "([^"]*)" "([^"]*)"', comment.body)
+            if match:
+                duration = match.group(1)
+                reason = match.group(2)
+                msg = match.group(3)
+                logging.debug('Ban (%s: %s -- %s) matched.', duration, reason,
+                              msg)
+                parent = self.r.get_info(thing_id=comment.parent_id)
+                comment.remove()
+                parent.remove()
+                self.r.get_subreddit(subreddit).add_ban(
+                    parent.author.name, duration=duration, note=reason,
+                    ban_message=msg)
+                logging.debug('User banned.')
+
+    def check_mail(self):
+        logging.debug('Checking mail…')
+        for mail in self.r.get_unread(True, True):
+            mail.mark_as_read()
+            logging.debug('New mail: "{}".'.format(mail.body))
+            match = re.search(r'@refresh (.*)', mail.body)
+            if not match:
+                continue
+            subreddit = match.group(1)
+            if subreddit in self.subreddits:
+                sub = self.subreddits[subreddit]
+                if mail.author.name in sub['mods']:
+                    self.refresh_sub(subreddit)
+                    self.r.send_message(
+                        mail.author.name, "Taskerbot refresh",
+                        "Refreshed mods and reasons for {}!".format(subreddit))
+                else:
+                    self.r.send_message(
+                        mail.author.name, "Taskerbot refresh",
+                        ("Unauthorized: not an r/{} mod").format(subreddit))
+            else:
+                self.r.send_message(mail.author.name, "Taskerbot refresh",
+                                    "Unrecognized sub:  {}.".format(subreddit))
+
+    def run(self):
+        while True:
+            logging.debug('Running cycle…')
+            for subreddit in SUBREDDITS:
+                try:
+                    self.check_comments(subreddit)
+                    self.check_mail()
+                except Exception as exception:
+                    logging.exception(exception)
+            logging.debug('Sleeping…')
+            time.sleep(32) # PRAW caches responses for 30s.
+
+
+if __name__ == '__main__':
+    with open('config.yaml') as config_file:
+        CONFIG = yaml.load(config_file)
+        USERNAME = CONFIG['Username']
+        PASSWORD = CONFIG['Password']
+        SUBREDDITS = CONFIG['Subreddits']
+        USER_AGENT = CONFIG['User Agent']
+
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s: %(message)s')
+    logging.getLogger('requests').setLevel(logging.WARNING)
+    MODBOT = Bot(Reddit(user_agent=USER_AGENT))
+    MODBOT.run()
